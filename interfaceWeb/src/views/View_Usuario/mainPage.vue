20ed69077860777b955aa42b7d59751ce3c37a65
Yuri Witer Rocha Barboza <yuri.barboza@sga.pucminas.br>
2022-09-28 02:05:59 -0300

    Update mainPage.v2
    Feito
     - Adicionado Navbar
     - Adicionado Campos
     - Adicionado Botões/Cards navegações
     - Instalado e configurado Material UI para dev
     - Adicionado pasta de assets
    
    Falta Fazer
     - Inserir mascaras campos
     - Retrabalho cards (Inserir Imagens,textos,rotas)
     - Alinhamento campos
     - Inserir parte dos exercicios

--- interfaceWeb/src/views/View_Usuario/mainPage.vue
+++ interfaceWeb/src/views/View_Usuario/mainPage.vue
@@ -1,37 +1,126 @@
 <template>
-  <div class="Geral">
+  <div>
+    <md-toolbar class="md-accent" md-elevation="1">
+      <h3 class="md-title" style="flex: 1">Bem-vindo, Senhor @</h3>
+      <md-button>Refresh</md-button>
+      <md-button class="md-primary">
+        <md-avatar>
+          <img src="https://images.unsplash.com/photo-1621750627159-cf77b0b91aac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9keWJ1aWxkZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="Avatar" />
+          <md-tooltip md-direction="top">Top</md-tooltip>
+        </md-avatar>
+      </md-button>
+    </md-toolbar>
     <div class="Topo">
-      <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <div class="text-overline mb-4">OVERLINE</div>
-            <v-list-item-title class="text-h5 mb-1">
-              Headline 5
-            </v-list-item-title>
-            <v-list-item-subtitle
-              >Greyhound divisely hello coldly
-              fonwderfully</v-list-item-subtitle
-            >
-          </v-list-item-content>
+      <md-card>
+        <md-card-media-cover md-text-scrim>
+          <md-card-media md-ratio="16:9">
+            <img
+              src="https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+              alt="Skyscraper"
+            />
+          </md-card-media>
 
-          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-        </v-list-item>
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Gradient background</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
 
-        <v-card-actions>
-          <v-btn outlined rounded text> Button </v-btn>
-        </v-card-actions>
-      </v-card>
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+      <md-card>
+        <md-card-media-cover md-text-scrim>
+          <md-card-media md-ratio="16:9">
+            <img
+              src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Skyscraper"
+            />
+          </md-card-media>
+
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Gradient background</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+      <md-card>
+        <md-card-media-cover md-text-scrim>
+          <md-card-media md-ratio="16:9">
+            <img
+              src="https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+              alt="Skyscraper"
+            />
+          </md-card-media>
+
+          <md-card-area>
+            <md-card-header>
+              <span class="md-title">Gradient background</span>
+              <span class="md-subhead">16/9 image</span>
+            </md-card-header>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
     </div>
+    <div class="Meio">
+      <md-field>
+        <label>Insira</label>
+        <md-input v-model="withLabel" placeholder="os carboidratos"></md-input>
+      </md-field>
 
-    <div class="Meio"></div>
-
-    <div class="Fim"></div>
+      <md-field>
+        <label>Insira</label>
+        <md-input v-model="withLabel" placeholder="proteinas"></md-input>
+      </md-field>
+      <md-field>
+        <label>Insira</label>
+        <md-input v-model="withLabel" placeholder="gorduras"></md-input>
+      </md-field>
+      <md-field>
+        <label>Insira</label>
+        <md-input v-model="withLabel" placeholder="Kcal"></md-input>
+      </md-field>
+      <md-button class="md-raised md-primary">Adicionar Alimento</md-button>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "App",
+};
 </script>
 
 
 <style scoped>
+.Topo {
+  margin-top: 5%;
+  margin-left: 30%;
+  display: flex;
+}
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+.Meio {
+  align-content: inline;
+  margin: 10%;
+}
 </style>
