425763a6b3cf349d791061c38df7e1a3ee4a5b5f
Sena <joaosenadainese@gmail.com>
2022-10-05 22:27:19 -0300

    Model Announcement

---
+++ src/core-backend/models/Announcement.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema({
+    description: {type: String, required: true},
+    date: {type: Date, default: () => Date.now()},
+})
+
+const AnnouncementModel = mongoose.model('Announcement', announcementSchema);
+
+module.exports.schema = announcementSchema;
+module.exports.Model = AnnouncementModel;
