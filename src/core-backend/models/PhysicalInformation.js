6fc93c98ddc0dfb064103b6e713bb1100f70fee3
Sena <joaosenadainese@gmail.com>
2022-10-05 22:27:57 -0300

    Model PhysicalInformation

---
+++ src/core-backend/models/PhysicalInformation.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const physicalInformationSchema = new mongoose.Schema({
+    geneticSex: {
+        type: String, 
+        required: true, 
+        enum:['male','female']
+    },
+    age: {type: Number, required: true},
+    heigth: {type: Number, required: true},
+    weight: {type: Number, required: true},
+    imc: {type: Number},
+    tbm: {type: Number},
+    dailyKcal: {type: Number},
+})
+
+//TODO: criar um middleware de save e update que atualiza IMC, TMB e Kcal sempre
+
+const PhysicalInformationModel = mongoose.model('PhysicalInformation', physicalInformationSchema);
+
+module.exports.schema = physicalInformationSchema;
+module.exports.Model = PhysicalInformationModel;
