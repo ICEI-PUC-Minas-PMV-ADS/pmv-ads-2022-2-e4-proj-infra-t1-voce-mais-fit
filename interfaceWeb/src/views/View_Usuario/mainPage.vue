28777304e9e7769ee9a4e411e7e071c7b57a984f
Iago Iann <iagoiann2@hotmail.com>
2022-10-20 21:28:29 -0300

    components

--- interfaceWeb/src/views/View_Usuario/mainPage.vue
+++ interfaceWeb/src/views/View_Usuario/mainPage.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
+    <resumo-Usuario/>
+    <start-Component/>
     
   </div>
 </template>
 <script>
+import resumoUsuario from '@/Utils/resumoUsuario.vue'
+import startComponent from '@/components/startComponent.vue'
 export default {
+  components:{
+    resumoUsuario,
+    startComponent
+  }
 
 };
 </script>
