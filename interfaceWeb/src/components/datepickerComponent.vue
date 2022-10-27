aaba8cfb065ecddf8e26721a3b6ec8c27bcc47eb
Yuri Witer Rocha Barboza <yuri.barboza@sga.pucminas.br>
2022-10-26 21:54:28 -0300

    Realizado criação dos campos Rotina

---
+++ interfaceWeb/src/components/datepickerComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+<script default >
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+* {
+  /* font-family: 'Roboto', sans-serif; */
+  left: 0px;
+  top: 0px;
+  border-radius: 0px;
+  background-color: #ab0139;
+  text-align: center;
+}
+</style>
