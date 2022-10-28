f72c7cd09e5efd085cd7078e7dec65f6536e3158
Iago Iann <iagoiann2@hotmail.com>
2022-10-27 21:14:26 -0300

    Profile

--- interfaceWeb/src/Utils/resumoUsuario.vue
+++ interfaceWeb/src/Utils/resumoUsuario.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="miniContainer">
+  <img src="@/assets/border.svg" class="border">
   <div class="perfil"> PERFIL</div>
+  <img src="@/assets/profile.svg" class="profile">
   <div class="profileName">
     IAGO IANN
+    <img src="@/assets/stars.svg" class="stars">
   </div>
   <div class="diet"> 
     <p> A DIETA É COMPLETAMENTE FUNCIONAL E OBRIGATÓRIA PARA SUA PERFOMANCE. </p>
     <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit</p>
   </div>
+  <p class="text"> SEJA FORTE </p>
+  <img src="@/assets/details1.svg" class="details1">
+  <img src="@/assets/details2.svg" class="details2">
     
   </div>
 </template>
@@ -20,25 +26,28 @@ export default {
 
 <style scoped>
 .miniContainer {
-  position: absolute;
-height: 300px;
+position: absolute;
+height: 370px;
 left: 50px;
 right: 50px;
+font-family: 'Agency FB';
+font-style: normal;
+
 
 background: #2B2024;
 box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.101961);
 
 }
 .perfil {
-  color: #FD0054;
-  position: absolute;
+color: #FD0054;
+position: absolute;
 height: 38px;
 left: 11.19%;
 right: 83.81%;
 top: calc(50% - 38px/2 - 116px);
 font-style: normal;
 font-weight: 700;
-font-size: 20px;
+font-size: 22px;
 line-height: 38px;
 letter-spacing: 1.92px;
 }
@@ -46,18 +55,20 @@ letter-spacing: 1.92px;
   position: absolute;
   height: 38px;
   left: 50%;
-  top: calc(58% - 38px/2 - 143px);
+  top: calc(50% - 38px/2 - 143px);
   font-style: normal;
   font-weight: 700;
   font-size: 25px;
   line-height: 38px;
   letter-spacing: 1.92px;
 
+
+
   color: #FD0054;
 }
 
 .diet {
-  position: absolute;
+position: absolute;
 height: 57px;
 left: 7.44%;
 right: 13.6%;
@@ -67,26 +78,80 @@ font-weight: 700;
 font-size: 30px;
 line-height: 57px;
 
+
 color: #FAFAFA;
   
 }
 
 .lorem {
-  position: absolute;
+position: absolute;
 height: 151px;
 left: 30.55%;
 right: 23.05%;
-top: calc(50% - 151px/2 + 100.5px);
-font-style: normal;
+top: calc(90% - 151px/2 + 100.5px);
+font-style: oblique;
 font-weight: 400;
 font-size: 20px;
 line-height: 28px;
 letter-spacing: 1.44px;
 
+
 color: #FAFAFA;
 
 mix-blend-mode: normal;
 opacity: 0.6;
+}
+
+.stars {
+  position: absolute;
+  top: 40px;
+  right: -10px;
+  
+}
+
+.profile {
+ position: absolute;
+ left: 82vh;
+ padding: 10px;
+}
+
+.border {
+  position: absolute;
+  top: 65px;
+  left: 135px;
+}
+
+.text {
+position: absolute;
+height: 24px;
+left: 75.55%;
+right: 8.84%;
+top: calc(50% - 24px/2 + 83px);
+
+font-family: 'Agency FB';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 24px;
+color: #FD0054;
+
+
+}
+
+.details1 {
+position: absolute;
+width: 34.3px;
+height: 8.94px;
+right: 315px;
+top: calc(51% - 8.94px/2 + 96.92px);
+}
+
+.details2 {
+position: absolute;
+width: 34.3px;
+height: 8.94px;
+left: 153vh;
+top: calc(52% - 8.96px/2 + 96.92px);
 }
 
 
