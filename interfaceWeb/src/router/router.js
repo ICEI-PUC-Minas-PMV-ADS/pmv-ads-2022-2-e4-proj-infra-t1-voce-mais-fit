1c9dd5881a2d895ace9b4612c363de3971ce8c7a
Felipe <fbvmcosta@sga.pucminas.br>
2022-10-28 12:47:47 -0300

    Início User Config Page
    Início User Config Page

--- interfaceWeb/src/router/router.js
+++ interfaceWeb/src/router/router.js
@@ -6,6 +6,7 @@ import treinoPage from "@/views/View_Treino/treinoPage";
 import loginPage from "@/views/Login/loginPage";
 import registerUserPage from "@/views/Register/registerUserPage"
 import registerPersonal from "@/views/Register/registerPersonal"
+import UserConfig from "@/views/User_Config/UserConfigPage"
 
 Vue.use(Router);
 
@@ -36,5 +37,9 @@ export default new Router({
             path:"/registerPersonal",
             component: registerPersonal
         },
+        {
+            path:"/userconfig",
+            component: UserConfig
+        }
     ] 
 })
---
+++ interfaceWeb/src/views/User_Config/UserConfigPage.vue
@@ -0,0 +1,223 @@
+<template>
+    <div>
+        <headerComponent></headerComponent>
+        
+        <div class="formContainer">
+                <div class="row" id="row">
+                    <div class="col-6" >
+
+                        <label class="mb-1">{{nome}}</label>
+                        <br>
+                        <p v-if="displayNome"><input id="inputForm" placeholder="Digite o nome que deseja alterar" type="text" class="input_default form-control requiredName" v-model="userName" /></p>
+                        <button type="button" class="btn btn-light" @click="AlteraDisplay">{{ ButtonName }}</button>
+                        
+                    </div>
+                    <div class="col-6" >
+
+                        <label class="mb-1">{{cpf}}</label>
+                        <br>
+                        <p v-if="displayCpf"><input id="inputForm" placeholder="000.000.000-00" type="Seu CPF" class="input_default form-control requiredName" v-mask="'###.###.###-##'" v-model="userCpf" @input="validaCpf"/></p>
+                        <button type="button" class="btn btn-light" @click="AlteraDisplayCpf">{{ ButtonCpf }}</button>
+                        
+                    </div>
+                    <div class="col-6" >
+
+                        <label class="mb-1">{{email}}</label>
+                        <br>
+                        <p v-if="displayEmail"><input id="inputForm" placeholder="Seu e-mail" type="email" class="input_default form-control requiredName" v-model="userEmail" @input="validaMail"/></p>
+                        
+                        <button type="button" class="btn btn-light" @click="AlteraDisplayEmail">{{ ButtonEmail }}</button>
+
+                    </div>
+                    <div class="col-6" >
+
+                        <label class="mb-1">{{senha}}</label>
+                        <br>
+                        <p v-if="displaySenha"><input id="inputFormTwo" placeholder="Sua Senha" type="password" class="input_default form-control required" v-model="userSenha" @input="validaSenha"/></p>
+                        
+                        <button type="button" class="btn btn-light" @click="AlteraDisplaySenha">{{ ButtonSenha }}</button>
+
+                    </div>
+
+                </div>
+            </div>
+
+        <footerComponent></footerComponent>
+    </div>
+    
+
+</template>
+
+
+<script>
+
+    import headerComponent from '@/components/headerComponent.vue'
+    import footerComponent from '@/components/footerComponent.vue'
+
+    export default {
+        components: {
+            headerComponent,
+            footerComponent,
+        },
+        data(){
+            return{
+                nome: "nome Exp",
+                cpf: "00000000000",
+                email: "exp@exp",
+                senha: "******",
+
+                displayNome: false,
+                displayCpf: false,
+                displayEmail: false,
+                displaySenha: false,
+
+                ButtonName: "Alterar",
+                ButtonCpf: "Alterar",
+                ButtonEmail: "Alterar",
+                ButtonSenha: "Alterar",
+            }
+        },
+        methods:{
+            AlteraDisplay(){
+                this.displayNome = !this.displayNome;
+                if(this.displayNome)
+                    this.ButtonName = "Cancelar"
+                else{
+                    this.ButtonName = "Alterar"
+                }
+            },
+
+            AlteraDisplayCpf(){
+                this.displayCpf = !this.displayCpf;
+                if(this.displayCpf)
+                    this.ButtonCpf = "Cancelar"
+                else{
+                    this.ButtonCpf = "Alterar"
+                }
+            },
+
+            AlteraDisplayEmail(){
+                this.displayEmail = !this.displayEmail;
+                if(this.displayEmail)
+                    this.ButtonEmail = "Cancelar"
+                else
+                    this.ButtonEmail = "Alterar"
+            },
+
+            AlteraDisplaySenha(){
+                this.displaySenha = !this.displaySenha;
+                if(this.displaySenha)
+                    this.ButtonSenha = "Cancelar"
+                else
+                    this.ButtonSenha = "Alterar"
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+
+headerMiniLogo{
+    width: 40%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #222222;
+    text-decoration: none;
+}
+.headerLink{
+    width: 60%;
+    height: 100%;
+}
+.headerLoginContainer{
+    width: 100%;
+    height: 221px;
+    background-color: #601430;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.headerLogin{
+    width: 60%;
+    height: 140px;
+    background-color: #FD0054;
+    display: flex;
+    flex-direction: row;
+}
+.headerLink{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+.link{
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #FFFF;
+    text-decoration: none;
+    font-size:max(2.5vh, 16px) ;
+}
+.link:hover{
+    color: rgb(154, 154, 154);
+    border-bottom: 1px rgb(154, 154, 154) solid;
+    transition: 0.7s;
+}
+.textLogo{
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #FFFF;
+    text-decoration: none;
+    font-size:max(2.5vh, 16px) ;
+}
+
+.allContainer{
+    display: flex;
+    flex-direction: column;
+    background-color: #601430;
+}
+
+.formContainer{
+    background: #601430;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+label{
+    color: #FFFF;
+}
+
+.forget{
+    color: #FFFF;
+}
+input{
+    margin-bottom: 1%;
+    height: 55px;
+}
+.col-6 :nth-child(1){
+    margin-top: 4%;
+}
+.row{
+    padding-top: 5%;
+    padding-bottom: 12%;
+    border: 4px #FD0054 solid;
+    width: 80%;
+    padding-bottom: 8%;
+    margin-bottom: 2%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+span{
+    display: none;
+    color: red;
+}
+.allLoginPageContainer{
+    background-color: #601430;
+}
+
+</style>
