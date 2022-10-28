43d9d692d818ea99a08c45d0ab8a4eb4b8593a0f
Iago Iann <iagoiann2@hotmail.com>
2022-10-27 21:22:11 -0300

    be continued

--- interfaceWeb/src/App.vue
+++ interfaceWeb/src/App.vue
@@ -16,10 +16,6 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-<<<<<<< HEAD
   
-=======
-   @import url(https://allfont.net/allfont.css?fonts=agency-fb);
->>>>>>> IagoIann
 }
 </style>
