e645123b9ec627bf91a984daa79dbc9634cbca02
Yuri Witer Rocha Barboza <yuri.barboza@sga.pucminas.br>
2022-10-28 23:07:03 -0300

    Instalação -  datepicker

--- interfaceWeb/src/components/datepickerComponent.vue
+++ interfaceWeb/src/components/datepickerComponent.vue
@@ -1,29 +1,47 @@
 <template>
- 
   <div class="calendar">
-    <label for="example-datepicker">Choose a date</label>
-    <b-calendar block locale="en-US"></b-calendar>
+    <p>Modelo de Exercícios</p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"></th>
+        </tr>
+        <tr>
+          <th scope="row"></th>
+        </tr>
+        
+        <tr>
+          <th scope="row"></th>
+        </tr>
+        
+        <tr>
+          <th scope="row"></th>
+        </tr>
+        
+      </tbody>
+    </table>
   </div>
-  
 </template>
 <script>
-
 </script>
 <style scoped>
-
-
 * {
   /* font-family: 'Roboto', sans-serif; */
   left: 0px;
   top: 0px;
-  border-radius: 0px;
- 
+  border-radius: 10px;
+
   text-align: center;
-  background-color: orangered;
+  /* background-color: orangered; */
   text-align: center;
-  
 }
-.calendar{
-  width: 700px;
+.calendar {
+  padding: 5% 5% 5% 5%;
+  width: 100%;
 }
 </style>
--- interfaceWeb/src/views/View_Usuario/mainPage.vue
+++ interfaceWeb/src/views/View_Usuario/mainPage.vue
@@ -4,6 +4,7 @@
     <startComponentVue id="home"></startComponentVue>
     <resumo-Usuario />
     <rotinaComponent id="rotina"/>
+    <datepickerComponent></datepickerComponent>
     <footerComponent></footerComponent>
   </div>
 </template>
@@ -13,13 +14,15 @@ import headerComponent from '@/components/headerComponent.vue'
 import footerComponent from '@/components/footerComponent.vue'
 import resumoUsuario from '@/Utils/resumoUsuario.vue';
 import rotinaComponent from '@/components/rotinaComponent.vue'
+import datepickerComponent from '@/components/datepickerComponent.vue'
 export default {
   components: {
     resumoUsuario,
     rotinaComponent,
     headerComponent,
     footerComponent,
-    startComponentVue
+    startComponentVue,
+    datepickerComponent
   },
 };
 </script>
