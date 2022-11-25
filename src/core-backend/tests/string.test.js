57e7711030642be93728f6fd4e5d565fe8bcb567
Sena <joaosenadainese@gmail.com>
2022-11-25 16:23:55 -0300

    testes string utils base

---
+++ src/core-backend/tests/string.test.js
@@ -0,0 +1,17 @@
+const stringUtils = require('../utils/string');
+
+describe('Normalize Text', () => {
+    it('should return a string in lowercase, without symbols, spaces, grammar marks', () => {
+        //todo: implement
+    })
+})
+
+describe('Generate Random Text', () => {
+    it('should return a string with same length as parameter', () => {
+        //todo: implement
+    })
+
+    it('should actual result be different of previous result', () => {
+        //todo: implement
+    })
+})
