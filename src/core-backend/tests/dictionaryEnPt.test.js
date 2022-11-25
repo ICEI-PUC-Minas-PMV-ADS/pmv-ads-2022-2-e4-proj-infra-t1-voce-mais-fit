dc1816d9534581fc5ddcd3b6f121fe665266b5ee
Sena <joaosenadainese@gmail.com>
2022-11-25 16:48:33 -0300

    testes dicionario base

---
+++ src/core-backend/tests/dictionaryEnPt.test.js
@@ -0,0 +1,21 @@
+const internalDictionary = require('../utils/dictionaryEnPt');
+
+describe('English to Portuguese', () => {
+    it('should return a string if a word that exists in the dictionary is given', () => {
+        //todo: implement
+    })
+
+    it('should return null if a word that does not exist in the dictionary is given', () => {
+        //todo: implement
+    })
+})
+
+describe('Portuguese to English', () => {
+    it('should return a string if a word that exists in the dictionary is given', () => {
+        //todo: implement
+    })
+
+    it('should return null if a word that does not exist in the dictionary is given', () => {
+        //todo: implement
+    })
+})
