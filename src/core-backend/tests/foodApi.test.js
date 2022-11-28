db968e829b74662cd329bb0912ce63b22f0b338d
Sena <joaosenadainese@gmail.com>
2022-11-28 08:52:37 -0300

    youtubeApi testes base

--- src/core-backend/tests/foodApi.test.js
+++ src/core-backend/tests/foodApi.test.js
@@ -2,14 +2,14 @@ const foodApi = require('../external-services/foodApi');
 
 describe('Search Food', () => {
     it('should return an arry of food passing a portuguese query', () => {
-
+        //todo: implement
     })
 
     it('should return an empty array if no food is found', () => {
-
+        //todo: implement
     })
 
     it('should return null if an error is thrown', () => {
-        
+        //todo: implement
     })
 });
---
+++ src/core-backend/tests/youtubeApi.test.js
@@ -0,0 +1,11 @@
+const youtubeApi = require('../external-services/foodApi');
+
+describe('Search Video', () => {
+    it('should return an URL to a youtube video, given a query', () => {
+        //todo: implement
+    })
+
+    it('should return null if an error is thrown', () => {
+        //todo: implement
+    })
+})
