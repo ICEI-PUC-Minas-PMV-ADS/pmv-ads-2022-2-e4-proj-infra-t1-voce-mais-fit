385392225b0b433b8032f96aee120a934fe295fa
Sena <joaosenadainese@gmail.com>
2022-11-28 08:57:33 -0300

    translateApi testes base

---
+++ src/core-backend/tests/translateApi.test.js
@@ -0,0 +1,15 @@
+const translateApi = require('../external-services/translateApi');
+
+describe('Portuguese to English', () => {
+    it('should return the english translation given a portuguese text', async () => {
+
+    })
+
+    it('should return the same text if no translation is available', () => {
+
+    })
+
+    it('should call API only if word is not found in internalDictionary', () => {
+        
+    })
+})
