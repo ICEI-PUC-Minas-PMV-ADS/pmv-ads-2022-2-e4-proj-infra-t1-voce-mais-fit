b4992898537a061c6ad0c2e5b100fcd77d0c878e
Sena <joaosenadainese@gmail.com>
2022-11-28 08:45:45 -0300

    foodApi teste base

---
+++ src/core-backend/tests/foodApi.test.js
@@ -0,0 +1,15 @@
+const foodApi = require('../external-services/foodApi');
+
+describe('Search Food', () => {
+    it('should return an arry of food passing a portuguese query', () => {
+
+    })
+
+    it('should return an empty array if no food is found', () => {
+
+    })
+
+    it('should return null if an error is thrown', () => {
+        
+    })
+});
