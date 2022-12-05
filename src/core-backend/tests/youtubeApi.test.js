d43de9bc21e407f31a5a33e881b9ea9f1d7a7a96
Sena <joaosenadainese@gmail.com>
2022-12-05 15:44:15 -0300

    youtubeApi testes implementação

--- src/core-backend/tests/youtubeApi.test.js
+++ src/core-backend/tests/youtubeApi.test.js
@@ -1,11 +1,24 @@
-const youtubeApi = require('../external-services/foodApi');
+const axios = require('axios');
+const youtubeApi = require('../external-services/youtubeApi');
 
 describe('Search Video', () => {
-    it('should return an URL to a youtube video, given a query', () => {
-        //todo: implement
+    it('should return an URL to a youtube video, given a query', async () => {
+        axios.request = jest.fn(() => {
+            return { data: { data: [{ videoId: "zwvsa7DzzHY"}] }};
+        })
+
+        let data = await youtubeApi.searchVideo('bambam enlouquecendo no treino de trapézio');
+
+        expect(data).toBe('https://www.youtube.com/watch?v=zwvsa7DzzHY');
     })
 
-    it('should return null if an error is thrown', () => {
-        //todo: implement
+    it('should return null if an error is thrown', async () => {
+        axios.request = jest.fn(() => {
+            throw {response: {data: {message: "expection test"}}};
+        })
+
+        let data = await youtubeApi.searchVideo('bambam enlouquecendo no treino de trapézio');
+
+        expect(data).toBeNull();
     })
 })
