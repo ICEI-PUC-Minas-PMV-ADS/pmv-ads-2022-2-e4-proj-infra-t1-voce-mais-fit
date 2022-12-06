4d973d0665879a8ddac79bba16500cf2bb33b7be
Sena <joaosenadainese@gmail.com>
2022-12-06 09:02:04 -0300

    translateApi test implementação

--- src/core-backend/tests/translateApi.test.js
+++ src/core-backend/tests/translateApi.test.js
@@ -1,15 +1,25 @@
 const translateApi = require('../external-services/translateApi');
+const axios = require("axios");
 
 describe('Portuguese to English', () => {
     it('should return the english translation given a portuguese text', async () => {
-        //todo: implement
+        axios.request = jest.fn(() => {
+            return {data: {ok: true, translated_text: 'scrambled eggs'}}
+        });
+
+        let data = await translateApi.portugueseToEnglish('ovos mexidos');
+        
+        expect(data).toBe('scrambled eggs');
+
     })
 
-    it('should return the same text if no translation is available', () => {
-        //todo: implement
-    })
+    it('should return the same text if no translation is available', async () => {
+        axios.request = jest.fn(() => {
+            return {data: {ok: false, translated_text: ''}}
+        });
 
-    it('should call API only if word is not found in internalDictionary', () => {
-        //todo: implement
+        let data = await translateApi.portugueseToEnglish('teste teste teste');
+        
+        expect(data).toBe('teste teste teste');
     })
 })
